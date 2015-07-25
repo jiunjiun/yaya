271942e2d634030401644dff70c48aa3c12ca842
jiunjiun <quietmes@gmail.com>
2015-07-26 00:46:43 +0800

    Add validates

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,8 @@
 class Destination < ActiveRecord::Base
+  validates_presence_of :iata, :en, :zh
+  validates_length_of :iata, is: 3
+
+  def self.find_or_create(destination = {})
+    create_with(en: destination.en, zh: destination.zh).find_or_create_by(iata: destination.iata) if destination.iata.present?
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,17 @@
 class Flight < ActiveRecord::Base
   belongs_to :destination, class_name: :Destination
   belongs_to :other_route, class_name: :Destination
+
+  validates_presence_of :terminal, :kind, :code, :code_zh,
+                        :flight, :gate, :datetime, :expected_datetime,
+                        :destination_id, :flight_status
+
+  validates_length_of :terminal, is: 2
+  validates_length_of :code, is: 3
+  validates_length_of :flight, maximum: 4
+  validates_length_of :gate, maximum: 3
+  validates_length_of :aircraft_type, maximum: 8
+
+  validates_inclusion_of :terminal, :in => ["T1", "T2"]
+  validates_inclusion_of :kind, :in => ["Arrivals", "Departure"]
 end
