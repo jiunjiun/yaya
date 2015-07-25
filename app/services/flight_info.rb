b6b5377f157a533768c4c79afde26ee60627dc11
jiunjiun <quietmes@gmail.com>
2015-07-26 00:47:42 +0800

    Add flight info and add save_yesterday_info method

--- app/services/flight_info.rb
+++ app/services/flight_info.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby
 
 class FlightInfo
+  def self.save_yesterday_info
+    haha = Alohaha.new
+    haha.flights.each do |flight|
+      if flight.datetime > Date.yesterday && flight.datetime < Date.today
+        destination = Destination.find_or_create(flight.destination)
+        other_route = Destination.find_or_create(flight.other_route)
 
+        flight_filter = flight.to_hash.reject {|key, value| key == :raw || key == :destination || key == :other_route}
+        flight_info = Flight.new(flight_filter)
+        flight_info.destination = destination
+        flight_info.other_route = other_route
+        flight_info.save
+      end
+    end
+  end
 end
