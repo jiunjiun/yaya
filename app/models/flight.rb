26e32add6f7e8e2367dec09bc4a61fbc04dad6b4
jiunjiun <quietmes@gmail.com>
2015-07-26 00:52:30 +0800

    Update validate

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,7 +7,7 @@ class Flight < ActiveRecord::Base
                         :destination_id, :flight_status
 
   validates_length_of :terminal, is: 2
-  validates_length_of :code, is: 3
+  validates_length_of :code, is: 2
   validates_length_of :flight, maximum: 4
   validates_length_of :gate, maximum: 3
   validates_length_of :aircraft_type, maximum: 8
