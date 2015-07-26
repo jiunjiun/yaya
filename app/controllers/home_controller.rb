c99f79b76e44a5f1c5cf167fcb22094b59e28000
jiunjiun <quietmes@gmail.com>
2015-07-26 18:28:32 +0800

    flight arrivals, departure top 20

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    flights = FlightInfo.flights
+    @flight_arrivals = flights[:Arrivals].take(20)
+    @flight_departure = flights[:Departure].take(20)
   end
 end
