919af9b79fe1e6417dbbe32da7b349d9c9272f83
jiunjiun <quietmes@gmail.com>
2015-07-31 00:02:52 +0800

    Add active_hash

---
+++ app/models/key_values.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+module KeyValues
+
+  # NEVER change the existed code sequence
+  class Base < ActiveHash::Base
+    def self.options
+      all.map { |t| [t.name, t.code] }
+    end
+
+    # {code1: name1, code2: name2}
+    def self.hash
+      Hash[*(all.map { |t| [t.code, t.name] }.flatten)]
+    end
+
+  end
+
+  class FlightKind < KeyValues::Base
+    self.data = [
+      {id: 1, name: 'Arrivals', code: :A },
+      {id: 2, name: 'Departure', code: :D },
+    ]
+  end
+
+  class FlightStatus < KeyValues::Base
+    self.data = [
+      {id: 1, zh: '準時', en: 'ON TIME', desc: '準時ON TIME'},
+      {id: 2, zh: '出發', en: 'DEPARTED', desc: '出發DEPARTED'},
+      {id: 3, zh: '延遲', en: 'DELAY', desc: '延遲DELAY'},
+      {id: 4, zh: '時間更改', en: 'SCHEDULE CHANGE', desc: '時間更改SCHEDULE CHANGE'},
+      {id: 5, zh: '取消', en: 'CANCELLED', desc: '取消CANCELLED'},
+      {id: 6, zh: '已到', en: 'ARRIVED', desc: '已到ARRIVED'},
+    ]
+  end
+end
