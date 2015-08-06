a17e981d5bae9880a1a0b605e45a48e39f0a21e0
jiunjiun <quietmes@gmail.com>
2015-08-06 21:11:37 +0800

    Update home field and helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def flight_code(code, flight)
-    "#{code}-#{flight}"
-  end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,21 @@
 module HomeHelper
+  def flight_code(code, flight)
+    "#{code}-#{flight}"
+  end
+
   def flight_static_desc(raw_desc)
     KeyValues::FlightStatus.find_by_desc(raw_desc).zh
+  end
+
+  def flight_code_name(flight)
+    flight_code = "#{flight.code}-#{flight.flight}"
+    "#{flight.code_zh} #{flight_code}"
+  end
+
+  def time_status(time, status)
+    short_time = l(time, format: :short)
+    static_desc = flight_static_desc(status)
+
+    "#{static_desc} #{short_time}"
   end
 end
