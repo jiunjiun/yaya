1ac6df17bccbec47acb05f255636b02dd44caf78
jiunjiun <quietmes@gmail.com>
2015-08-09 23:03:17 +0800

    Add search page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'departures/today'
   get 'departures/tomorrow'
 
+  resources :searchs, only: [:index]
+
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
