3c8e3304c23369c8d326cbdb70109e92b5e7e9ad
jiunjiun <quietmes@gmail.com>
2015-09-19 23:41:45 +0800

    fix query

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -14,13 +14,11 @@ class ChartsController < ApplicationController
       arrivals_t1 = arrivals.where(terminal: 'T1')
       arrivals_t2 = arrivals.where(terminal: 'T2')
 
-      departure = Flight.where("datetime >= ? and datetime < ?", date.beginning_of_day, (date+1).beginning_of_day ).where(kind: 'A').where(kind: 'D')
+      departure = Flight.where("datetime >= ? and datetime < ?", date.beginning_of_day, (date+1).beginning_of_day ).where(kind: 'D')
 
       departure_t1 = departure.where(terminal: 'T1')
       departure_t2 = departure.where(terminal: 'T2')
 
-
-      arrdeps = ChartTools.time_line(departure_t1)
 
       result = {
         arrivals: ChartTools.time_line(arrivals).values,
